deeba576415b7d0695a2a003e1de0628b7ba41ab
Adriano Reis <adriano16_gubert@hotmail.com>
2022-09-29 20:22:42 -0300

    v1

---
+++ lib/main.dart
@@ -0,0 +1,105 @@
+import 'package:flutter/material.dart';
+
+import 'screens/tabs_screen.dart';
+import 'screens/categories_meals_screen.dart';
+import 'screens/meal_detail_screen.dart';
+import 'screens/settings_screen.dart';
+import 'models/settings.dart';
+import 'models/meal.dart';
+import 'utils/app_routes.dart';
+import 'data/dummy_data.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  Settings settings = Settings();
+  List<Meal> _availableMeals = DUMMY_MEALS;
+  List<Meal> _favoriteMeals = [];
+
+  void _filterMeals(Settings settings) {
+    setState(() {
+      this.settings = settings;
+      _availableMeals = DUMMY_MEALS.where((meal) {
+        final filterGluten = settings.isGlutenFree && !meal.isGlutenFree;
+        final filterLactose = settings.isLactoseFree && !meal.isLactoseFree;
+        final filterVegan = settings.isVegan && !meal.isVegan;
+        final filterVegetarian = settings.isVegetarian && !meal.isVegetarian;
+        return !filterGluten &&
+            !filterLactose &&
+            !filterVegan &&
+            !filterVegetarian;
+      }).toList();
+    });
+  }
+
+  void _toggleFavorite(Meal meal) {
+    setState(() {
+      _favoriteMeals.contains(meal)
+          ? _favoriteMeals.remove(meal)
+          : _favoriteMeals.add(meal);
+    });
+  }
+
+  bool _isFavorite(Meal meal) {
+    return _favoriteMeals.contains(meal);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSwatch().copyWith(
+          primary: Colors.green,
+          secondary: Colors.amber,
+        ),
+        canvasColor: const Color.fromARGB(255, 219, 255, 222),
+        fontFamily: 'Raleway',
+        textTheme: ThemeData.light().textTheme.copyWith(
+              headline6: const TextStyle(
+                fontSize: 20,
+                fontFamily: 'RobotoCondensed',
+              ),
+            ),
+      ),
+      // darkTheme: ThemeData.dark(),
+      routes: {
+        AppRoutes.HOME: (ctx) => TabsScreen(_favoriteMeals),
+        AppRoutes.CATEGORIES_MEALS: (ctx) =>
+            CategoriesMealsScreen(_availableMeals),
+        AppRoutes.MEAL_DETAIL: (ctx) => MealDetailScreen(_toggleFavorite, _isFavorite),
+        AppRoutes.SETTINGS: (ctx) => SettingsScreen(settings, _filterMeals),
+      },
+
+      //Exemplos de rota determinada, se foi colocado determinado padrão va para determinada rota!
+      //Se não encontrar as rotas pré definidas o onGenerateRoute é acionado!
+      // onGenerateRoute: (settings) {
+      //   if (settings.name == '/nome_rota1') {
+      //     return null;
+      //   } else if (settings.name == 'nome_rota2') {
+      //     return null;
+      //   } else {
+      //     return MaterialPageRoute(
+      //       builder: (_) {
+      //         return const CategoriesScreen();
+      //       },
+      //     );
+      //   }
+      // },
+      //Esse metódo é acionado se nenhuma rota foi encontrada, ideal para mostrar pagina de erro ou não encontrado!
+      // onUnknownRoute: (settings) {
+      //   return MaterialPageRoute(
+      //     builder: (_) {
+      //       return const CategoriesScreen();
+      //     },
+      //   );
+      // },
+    );
+  }
+}
